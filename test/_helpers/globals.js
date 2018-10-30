b15e53b914df771372bf2641fa8efe55011d425d
goya <brettrudd@gmail.com>
2018-10-30 13:28:46 -0700

    fix tests

--- test/_helpers/globals.js
+++ test/_helpers/globals.js
@@ -1,3 +1,4 @@
 // mock fs so no tests accidently write to disk
 jest.mock('fs', () => require('jest-plugin-fs/mock'))
 process.on('unhandledRejection', console.error)
+delete process.env['PGB_API_URL']
